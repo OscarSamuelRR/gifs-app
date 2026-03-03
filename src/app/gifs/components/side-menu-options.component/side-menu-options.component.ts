ae937a61a68e9d12147848104cd41744c2ae9112
OscarSamuelRR <oscarsamuel524@gmail.com>
2026-03-02 22:05:55 -0600

    Router links, environments y Path

--- src/app/gifs/components/side-menu-options.component/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu-options.component/side-menu-options.component.ts
@@ -1,10 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+interface MenuOption{
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-menu-options',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './side-menu-options.component.html',
 })
 export class SideMenuOptionsComponent { 
-
+  menuOptions: MenuOption[] = [{
+    icon: 'fa-solid fa-chart-line',
+    label: 'Trending',
+    subLabel: 'Gifs Populares',
+    route: '/dashboard/trending'
+  },
+  {
+    icon: 'fa-solid fa-magnifying-glass',
+    label: 'Buscador',
+    subLabel: 'Buscar gifs',
+    route: '/dashboard/search'
+  }
+]
 }
